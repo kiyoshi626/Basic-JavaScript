e59711121f45bc62427cb916aa034ff379871310
Vance Villanueva <vancervillanueva@gmail.com>
2017-07-13 06:25:33 -0700

    Completed to verifyPassword.  6 more to go.

--- src/project-3.js
+++ src/project-3.js
@@ -5,50 +5,82 @@ const makeCat = (name, age) => {
   // add an age property to the object with the value set to the age argument
   // add a method called meow that returns the string 'Meow!'
   // return the object
+  const obj = {
+    name,
+    age,
+    meow(meow) {
+      return 'Meow!';
+    },
+  };
+  return obj;
 };
 
 const addProperty = (object, property) => {
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+  object[property] = null;
+  return object;
 };
 
 const invokeMethod = (object, method) => {
   // method is a string that contains the name of a method on the object
   // invoke this method
   // nothing needs to be returned
+  object[method]();
 };
 
 const multiplyMysteryNumberByFive = (mysteryNumberObject) => {
   // mysteryNumberObject has a property called mysteryNumber
   // multiply the mysteryNumber property by 5 and return the product
+  return mysteryNumberObject.mysteryNumber *= 5;
 };
 
 const deleteProperty = (object, property) => {
   // remove the property from the object
   // return the object
+  delete object[property];
+  return object;
 };
 
 const newUser = (name, email, password) => {
   // create a new object with properties matching the arguments passed in.
   // return the new object
+  const obj = {
+    name,
+    email,
+    password,
+  };
+  return obj;
 };
 
 const hasEmail = (user) => {
   // return true if the user has a value for the property 'email'
   // otherwise return false
+  if (user.email) {
+    return true;
+  }
+  return false;
 };
 
 const hasProperty = (object, property) => {
   // return true if the object has the value of the property argument
   // property is a string
   // otherwise return false
+  if (object[property]) {
+    return true;
+  }
+  return false;
 };
 
 const verifyPassword = (user, password) => {
   // check to see if the provided password matches the password property on the user object
   // return true if they match
   // otherwise return false
+  if (user[password] === password) {
+    return true;
+  }
+  return false;
 };
 
 const updatePassword = (user, newPassword) => {
